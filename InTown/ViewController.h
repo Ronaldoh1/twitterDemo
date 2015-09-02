db02f88b61da2720710a49d9100db19134e4b0b4
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-09-01 20:41:15 -0700

    Initial Commit

---
+++ InTown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InTown
+//
+//  Created by Ronald Hernandez on 9/1/15.
+//  Copyright (c) 2015 inTown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
